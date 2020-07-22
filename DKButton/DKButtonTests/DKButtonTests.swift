55d22ee6bb2cfa8f791a28190fe96bd2b7446c64
Dheeraj Kumar <dheerajkmar@gmail.com>
2020-07-22 12:57:42 +0530

    Added test cases

--- DKButton/DKButtonTests/DKButtonTests.swift
+++ DKButton/DKButtonTests/DKButtonTests.swift
@@ -11,24 +11,18 @@ import XCTest
 
 class DKButtonTests: XCTestCase {
 
+    var math: DKMath!
+
     override func setUp() {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+        math = DKMath()
     }
 
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    func testAdd() {
+        XCTAssertEqual(math.add(a: 1, b: 1), 2)
     }
-
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
+    
+    func testSub() {
+        XCTAssertEqual(math.sub(a: 2, b: 1), 1)
     }
 
 }
